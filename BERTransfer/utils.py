f38c8e02a1ca886a088972961882491b461129e2
Pclanglais <42674826+Pclanglais@users.noreply.github.com>
2023-04-04 13:40:35 +0200

    Update utils.py

--- BERTransfer/utils.py
+++ BERTransfer/utils.py
@@ -1,6 +1,6 @@
 #The function to create the BERTopic model.
 #The main difference with the official BERTopic implementation is that we return also the document embeddings that will be necessary for further calculations
-def create_bertopic(docs, language = "english", calculate_probabilities=True, verbose=True, bert_model = "all-MiniLM-L6-v2", similarity_threshold = 0.01, document_selection = 20):
+def create_bertopic(ids, docs, language = "english", calculate_probabilities=True, verbose=True, bert_model = "all-MiniLM-L6-v2", similarity_threshold = 0.01, document_selection = 20):
   from bertopic import BERTopic
   from sentence_transformers import SentenceTransformer
   from BERTransfer import BERTopicM
@@ -11,5 +11,5 @@ def create_bertopic(docs, language = "english", calculate_probabilities=True, ve
   # Train our topic model using our pre-trained sentence-transformers embeddings
   topic_model = BERTopic(language=language, calculate_probabilities=True, verbose=True)
   topics, probs = topic_model.fit_transform(docs, embeddings)
-  bertopic_model = BERTopicM(topic_model = topic_model, topics = topics, probs = probs, embeddings = embeddings, similarity_threshold = similarity_threshold, document_selection = document_selection)
+  bertopic_model = BERTopicM(ids = ids, topic_model = topic_model, topics = topics, probs = probs, embeddings = embeddings, similarity_threshold = similarity_threshold, document_selection = document_selection)
   return bertopic_model
